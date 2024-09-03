e7ddfde79d6ce2869e21ab3390488d1d99e274d5
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2024-09-03 12:13:39 +0200

    ** add ** set up front end

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/messages', [HomeController::class, 'messages'])->name('messages');
-Route::post('/message', [HomeController::class, 'message'])->name('message');
-
-?>
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])
+    ->name('home');
+Route::get('/messages', [HomeController::class, 'messages'])
+    ->name('messages');
+Route::post('/message', [HomeController::class, 'message'])
+    ->name('message');
