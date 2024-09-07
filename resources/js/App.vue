889c3ea867a43bbad157d63305e1dbf523739f23
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2024-09-07 09:19:57 +0200

    ** add ** added first version of the UI

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    isAuth: {
+        type: Boolean,
+        default: false
+    },
+    user: {
+        type: [Object, Array],
+        required: false
+    }
+})
+</script>
+<template>
+    <h1>Hello, {{ user.name }}</h1>
+</template>
